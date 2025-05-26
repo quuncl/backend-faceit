a948fbb3abacc71ef2de0bbfde9ef12eb3221f2e
valeriaVsavina <kirill56036@yandex.ru>
2025-05-26 08:27:36 +0300

    chore: remove backend for clean redeploy

--- backend/prisma/migrations/20250514064036_add_status_to_users/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `users` ADD COLUMN `status` INTEGER NOT NULL DEFAULT 0;
--- backend/prisma/migrations/20250526043639_add_bot_username_to_telegram/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `telegram` ADD COLUMN `botUsername` VARCHAR(191) NOT NULL DEFAULT 'verif_faceitbot';
